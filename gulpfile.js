7d89c23240b97ab93c9a886c57acb18e537de33b
imada shodai <shoday10@gmail.com>
2021-09-04 12:27:53 +0900

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const { src, dest, watch } = require("gulp");
+var sass = require('gulp-sass')(require('sass'));
+
+const compileSass = () =>
+  src("src/sass/**/*.scss")
+    .pipe(
+      sass({
+        outputStyle: "expanded"
+      })
+    )
+    .pipe(dest("public/css"));
+
+const watchSassFiles = () => watch("src/sass/**/*.scss", compileSass);
+
+exports.default = watchSassFiles;
