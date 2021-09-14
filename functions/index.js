8bc5509ba98024aa8aeca423375ce4829b51ef1c
imada shodai <shoday10@gmail.com>
2021-09-14 14:40:11 +0900

    change basic auth id pass

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,7 @@ const basicAuth = require('basic-auth-connect')
 const app = express()
 
 app.all('/*', basicAuth(function(user, password) {
-  return user === 'sepal' && password === 'sepaltest';
+  return user === 'uchimura-live2021' && password === 'kqU@qs3Y';
 }));
 
 app.use(express.static(__dirname + '/public/'))
